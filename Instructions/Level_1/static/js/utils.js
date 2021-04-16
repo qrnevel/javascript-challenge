72285dd4b0dc31cc114fa13e5203aaa9aa823b21
Q Nevels <76258341+qrnevel@users.noreply.github.com>
2021-04-15 19:30:36 -0500

    utils

--- Instructions/Level_1/static/js/utils.js
+++ Instructions/Level_1/static/js/utils.js
@@ -1,4 +1,11 @@
-// from data.js
-var tableData = data;
+// Function to create/populate table with data.
+const createTable = (data, tableBody) => {
+  // Loop through the items in the data array.
+  data.forEach(ufo => {
+    // Insert a row in the table at the last row
+    let newRow = tableBody.append('tr');
 
-// YOUR CODE HERE!
+    // Insert a cell in the row at index, create text nodes for each, append a text node to the cell.
+    Object.entries(ufo).forEach(([key, value]) => newRow.append('td').text(value.toString()));
+  });
+};
